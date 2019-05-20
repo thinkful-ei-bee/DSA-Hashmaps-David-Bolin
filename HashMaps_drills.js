6381f1e226fe5086cafbf2cd613d56a5614e5375
David Bolin <david@bolin.at>
2019-05-20 09:18:14 -0700

    answering 2 in comments

--- HashMaps_drills.js
+++ HashMaps_drills.js
@@ -27,6 +27,20 @@ function main() {
   // Maiar is Sauron and Hobbit is Frodo since these are the last values we wrote to these keys.
 
   // Capacity is 24 since we have a length of 9. Capacity was tripled when we went over a length of 4.
+
+  // Exercise 2
+  // The code prints the values 20, 10.
+  //
+  // The code creates two hashmaps each with the key 'Hello World.'
+  // The first map first has the value 10 assigned to this key, then overwrites this value with 20.
+  // The second map first has the value 20 assigned to it, then overwrites this with 10. Then the value of the key for each map is displayed.
+
+  // Exercise 3
+  //
+  // (1)
+  //
+  // 
+
 }
 
 main();
